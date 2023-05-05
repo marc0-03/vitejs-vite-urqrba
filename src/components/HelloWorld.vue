88e24607885a9ee704bb5e9ed7dd58a6c7b63d55
marc0-03 <60389027+marc0-03@users.noreply.github.com>
2023-05-05 16:36:37 +0200

    1 binaries uploaded

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,70 +6,33 @@ defineProps({
 });
 
 const count = ref(0);
-
-const contacts = [
+const cars = [
   {
-    name: 'Luke Skywalker',
-    address: '644 Vermont Court, Freelandville, Kentucky, 2619',
-    img: '../public/Truck.png',
-  },
-  {
-    name: 'C-3PO',
-    address: '626 Carroll Street, Roulette, Ohio, 1477',
-    img: 'https://randomuser.me/api/portraits/men/1.jpg',
-  },
-  {
-    name: 'Obi-Wan Kenobi',
-    address: '887 Winthrop Street, Tryon, Florida, 3912',
-    img: 'https://randomuser.me/api/portraits/men/2.jpg',
-  },
-  {
-    name: 'Jabba Desilijic Tiure',
-    address: '286 NW. Shore St.Longwood, FL 32777',
-    img: 'https://randomuser.me/api/portraits/women/4.jpg',
-  },
-  {
-    name: 'Yoda',
-    address: '353 NW. Shore St.Longwood, FL 32778',
-    img: 'https://randomuser.me/api/portraits/men/5.jpg',
-  },
-  {
-    name: 'Darth Vader',
-    address: '265 NW. Shore St.Longwood, FL 32779',
-    img: '../src/assets/Truck.png',
+    name: "car1",
+    type: "Goods Transportation",
+    battery: "40",
+    eta: "17",
   },
 ];
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <va-scroll-container style="max-height: 200px" vertical>
+  <va-scroll-container style="max-height: 400px" vertical>
     <va-list>
-      <va-list-label> Contacts </va-list-label>
-
-      <va-list-item
-        v-for="(contact, index) in contacts"
-        :key="index"
-        class="list__item"
-      >
-        <va-list-item-section avatar>
-          <va-avatar>
-            <img :src="contact.img" alt="" />
-          </va-avatar>
+      <va-list-item v-for="car in cars" :key="index" class="list__item">
+        <va-list-item-section icon>
+          <img src="../assets/Truck.png" height="30">
         </va-list-item-section>
-
         <va-list-item-section>
           <va-list-item-label>
-            {{ contact.name }}
+            {{ car.name }}
           </va-list-item-label>
-
           <va-list-item-label caption>
-            {{ contact.address }}
+            {{ car.type }}
           </va-list-item-label>
         </va-list-item-section>
-
         <va-list-item-section icon>
-          <va-icon name="remove_red_eye" color="background-tertiary" />
+          <img src="../assets/Truck.png" height="30">
         </va-list-item-section>
       </va-list-item>
     </va-list>
@@ -87,5 +50,8 @@ const contacts = [
 <style lang="scss" scoped>
 .list__item + .list__item {
   margin-top: 20px;
+}
+.va-icon + .va-icon {
+  margin-left: 2rem;
 }
 </style>
